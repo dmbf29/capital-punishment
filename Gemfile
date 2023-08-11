dd6d6cda8836f2446b0dc715ccf4417a1e7c0b9b
Douglas Berkley <douglasmberkley@gmail.com>
2023-08-11 10:36:36 +0900

    added cloudinary with activestorage

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 gem "bootsnap", require: false
 
 # Use Sass to process CSS
+gem "cloudinary"
 gem "sassc-rails"
 gem "simple_form"
 
--- app/models/criminal.rb
+++ app/models/criminal.rb
@@ -2,5 +2,6 @@ class Criminal < ApplicationRecord
   belongs_to :prison # criminal.prison
   has_many :convictions, dependent: :destroy
   has_many :crimes, through: :convictions
+  has_one_attached :photo
   validates :name, presence: true
 end
